e2515d2d2b7157642f9175c536c26faae2394509
simizu-masato <m.shimizu@trigger4.com>
2024-05-06 18:02:40 +0900

    実装一覧作成

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Implementation {
+  title: string;
+  content: string;
+  path: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  implementations: Implementation[] = [
+    {
+      title: 'ユーザー一覧',
+      content: 'ユーザー一覧情報をAPIで取得し、表示する機能',
+      path: '/user',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
