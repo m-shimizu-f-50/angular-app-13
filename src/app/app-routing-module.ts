58c00193976fb0c616af56425a48ed1c3a3d777d
simizu-masato <m.shimizu@trigger4.com>
2024-05-07 23:03:34 +0900

    testコンポーネント作成

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { UsersComponent } from './pages/users/users.component';
+import { TestComponent } from './pages/test/test.component';
 
 // ルーティング設定
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'test', component: TestComponent },
   { path: 'users', component: UsersComponent },
   { path: '**', component: NotFoundComponent },
 ];
